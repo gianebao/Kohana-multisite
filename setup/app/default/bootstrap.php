adef5a47ca4bdb2bbbe0840bf2cb9c9151237533
Gian Carlo Val Ebao <gianebao@gmail.com>
2012-08-29 13:34:44 +0800

    fixed: unable to delete hidden files

--- setup/app/default/bootstrap.php
+++ setup/app/default/bootstrap.php
@@ -108,10 +108,10 @@ Kohana::modules(array(
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
 	// 'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
-	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	// 'unittest'   => MODPATH.'unittest',   // Unit testing
-	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	'layout'        => MODPATH.'layout',     // Segmented Templating System
+	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	'unittest'   => MODPATH.'unittest',      // Unit testing
+	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
 
 /**
